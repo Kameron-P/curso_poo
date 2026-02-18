cf0b5c9b292644ba80312aca4655a3d81d186f12
Kameron-P <kameron.pastrana@campusucc.edu.co>
2026-02-18 01:14:59 +0000

    Se quito la inicializacion  con 0 de las variables a y b

--- Clase3/Condicinales.java
+++ Clase3/Condicinales.java
@@ -4,7 +4,7 @@ public class Condicinales {
     public static void main (String[]args){
     //leer 2 numeros y deducir si estan en orden creciente. a<=b
     Scanner sc = new Scanner(System.in);  
-    int a = 0 , b=0;
+    int a   , b;
     System.out.print("Digite a:");
     a = sc.nextInt();//capturamos por teclado el valor de 'a'
     System.out.print("Digite b:");
