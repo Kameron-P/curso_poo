2d450857e26471b9aaf2eb4e983446ca218a7141
Kameron-P <kameron.pastrana@campusucc.edu.co>
2026-03-11 02:15:14 +0000

    Taller Momento 1

--- Ejercicios/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Libro libro1 = new Libro("Cien años de soledad", "Gabriel García Márquez", 471);
-        Libro libro2 = new Libro("1984", "George Orwell", 328);
-        libro1.mostrarInfo();
-        libro2.mostrarInfo();
-    }
-}
