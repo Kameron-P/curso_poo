95feb6b46317bb76a4c3c9a8a082d21ddbd27fd7
Kameron-P <kameron.pastrana@campusucc.edu.co>
2026-03-15 23:54:03 +0000

    Taller

--- Clase6/EjCuenta.java
+++ Clase6/EjCuenta.java
@@ -1,15 +1,10 @@
 public class EjCuenta {
     public static void main(String[] args) {
-        // Crear la instancia (el objeto)
         CuentaBancaria miCuenta = new CuentaBancaria("987654", "Maria Lopez", 1000.0);
+        System.out.println(miCuenta); 
 
-        // Realizar operaciones
-        System.out.println(miCuenta); // Consulta inicial
-
-        miCuenta.depositar(500.0);    // Depósito
-        miCuenta.retirar(200.0);     // Retiro
-        
-        // Consulta final
+        miCuenta.depositar(500.0);  
+        miCuenta.retirar(200.0);  
         System.out.println("Estado final: " + miCuenta);
     }
 }
