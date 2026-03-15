eb4f62cab609d4b3e46ebb45b145c9023d198af7
Kameron-P <kameron.pastrana@campusucc.edu.co>
2026-03-15 23:40:54 +0000

    Momento 1

--- Clase6/CuentaBancaria.java
+++ Clase6/CuentaBancaria.java
@@ -1,25 +1,20 @@
 public class CuentaBancaria {
-    // 1. Atributos (Lo que la cuenta "tiene")
+    //Atributos 
     public String numeroCuenta;
     public String titular;
     public double saldo;
 
-    // 2. Constructor (Para crear la cuenta con datos desde el inicio)
     public CuentaBancaria(String num, String nombre, double saldoInicial) {
         this.numeroCuenta = num;
         this.titular = nombre;
         this.saldo = saldoInicial;
-    }
-
-    // 3. Métodos (Lo que la cuenta "hace")
-    
+    }    
     public void depositar(double cantidad) {
-        saldo = saldo + cantidad; // Forma clásica de principiante
+        saldo = saldo + cantidad; 
         System.out.println("Has depositado: $" + cantidad);
     }
 
     public void retirar(double cantidad) {
-        // Validación básica para no quedar en negativo
         if (cantidad <= saldo) {
             saldo = saldo - cantidad;
             System.out.println("Has retirado: $" + cantidad);
@@ -28,6 +23,6 @@ public class CuentaBancaria {
         }
     }
     public String toString() {
-        return "Cuenta de " + titular + " - Saldo actual: $" + saldo;
+        return "CuentaBancaria: Cuenta de " + titular + " - Saldo actual: $" + saldo;
     }
 }
