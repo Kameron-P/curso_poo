d868e87062da5b33fa39b5378b2446de24f95e84
Kameron-P <kameron.pastrana@campusucc.edu.co>
2026-03-18 01:47:29 +0000

    Herencia

--- Clase7/Animal.java
+++ Clase7/Animal.java
@@ -1,9 +1,13 @@
+package Clase7;
 public class Animal {
     private String nombre;
     private int edad;
     public Animal(String nombre, int edad){
         this.nombre=nombre;
         this.edad=edad;
+    }
+    public String getNombre(){
+        return nombre;
     }
     public int getEdad(){
         return edad;
---
+++ Clase7/Perro.java
@@ -0,0 +1,33 @@
+package Clase7;
+public class Perro extends Animal {
+    
+    private String raza;
+
+    public Perro(String nombre, int edad, String raza) {
+        super(nombre, edad);
+        this.raza = raza;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(getNombre() + " dice: ¡Guau!");
+    }
+
+    public void buscarPelota() {
+        System.out.println(getNombre() + " fue a buscar la pelota.");
+    }
+
+    @Override
+    public String toString() {
+        return "Perro[ " + super.toString() + ", Raza=" + raza + "]";
+    }
+    
+}
