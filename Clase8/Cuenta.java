89194dbe8adeddbb3665ef69aa08015e22eb2c79
Kameron-P <kameron.pastrana@campusucc.edu.co>
2026-04-15 01:24:50 +0000

    Herencia

---
+++ Clase8/Cuenta.java
@@ -0,0 +1,22 @@
+public class Cuenta{
+    private String numeroCuenta;
+    private double saldo;
+
+    public Cuenta (String numeroCuenta, double saldo) {
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+    //geters
+    public String getNumeroCuenta(){
+        return numeroCuenta;
+    }
+    public double getSaldo(){
+        return saldo;
+    }
+    public double mostrarSaldo(){
+        return saldo;
+    }
+    public String toString(){
+        return "Cuenta{numeroCuenta:"+numeroCuenta+"Saldo:"+saldo+"};";
+    }
+}
